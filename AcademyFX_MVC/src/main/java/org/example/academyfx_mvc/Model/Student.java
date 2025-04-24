9e9f58ce0aa56c573510f11933aa5439b6839b79
Александр Архипов <liney13@mail.ru>
2025-04-24 18:56:24 +0800

    getDataFromBase in Human & Student DONE

--- AcademyFX_MVC/src/main/java/org/example/academyfx_mvc/Model/Student.java
+++ AcademyFX_MVC/src/main/java/org/example/academyfx_mvc/Model/Student.java
@@ -2,15 +2,20 @@ package org.example.academyfx_mvc.Model;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 import java.util.Date;
+
+//import jakarta.
 
 public class Student extends Human{
     private final IntegerProperty group;
 
-    public Student(Integer id, String lastName, String firstName, String middleName, Date birthDate, Integer group) {
+    public Student(Integer id, String lastName, String firstName, String middleName, Date birthDate, String groupName) {
         super(id, lastName, firstName, middleName, birthDate);
         this.group = new SimpleIntegerProperty(group);
+        this.groupName = new SimpleStringProperty(groupName);
     }
     public IntegerProperty groupProperty()
     {
@@ -23,5 +28,18 @@ public class Student extends Human{
     public final void setGroup(Integer group)
     {
         this.group.set(group);
+    }
+    /// ////////////////////
+    //Navigation properties:
+
+    private final StringProperty groupName;
+    public StringProperty groupNameProperty(){
+        return groupName;
+    }
+    public final String getGroupName(){
+        return groupName.get();
+    }
+    public final void setGroupName(String groupName){
+        this.groupName.set(groupName);
     }
 }
